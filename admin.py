acf4ab8abbf4afb7ea8fe36f2d42666bd6957ca5
sushantnair <sushantnair.developer@gmail.com>
2023-10-13 20:30:41 +0530

    removed files which I felt were not related to scfy. But backup has been kept in case of future reference requirement.

--- admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import UserInfo
-
-admin.site.register(UserInfo)
--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class UserInfo(models.Model):
-    user_name = models.CharField(max_length=100)
-    user_description = models.TextField()
-    user_image = models.ImageField(upload_to='users')
