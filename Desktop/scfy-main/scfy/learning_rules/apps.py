41b41e93e3508ba33a5cdd43ad4a6a14e68a796d
juniperus01 <jiyabhagat402@gmail.com>
2024-10-16 18:00:54 +0530

    Updating past_orders

--- Desktop/scfy-main/scfy/learning_rules/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LearningRulesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'learning_rules'
--- Desktop/scfy-main/scfy/learning_rules/urls.py
+++
@@ -1,6 +0,0 @@
-from . import views
-from django.urls import path
-urlpatterns = [
-    path("", views.allpages),
-    path("<lr_name>", views.learnrule, name="lr_name"),
-]   
